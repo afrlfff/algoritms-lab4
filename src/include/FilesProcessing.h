61972995b92526e203be7fc677042a7fe746c5fd
aFroloff <frolovalexander993@gmail.com>
2024-03-21 19:46:49 +0300

    Update FilesProcessing

--- src/include/FilesProcessing.h
+++ src/include/FilesProcessing.h
@@ -13,8 +13,22 @@ const std::wstring ReadWideStrBinary(FILE*& f, const size_t size);
 void AppendWideStrBinary(const std::wstring& str, FILE*& f);
 const wchar_t ReadWideCharBinary(FILE*& f);
 void AppendWideCharBinary(const wchar_t c, FILE*& f);
-const int ReadIntBinary(FILE*& f);
-void AppendIntBinary(const int number, FILE*& f);
+const int64_t ReadInt64Binary(FILE*& f);
+const uint64_t ReadUint64Binary(FILE*& f);
+void AppendInt64Binary(const int64_t number, FILE*& f);
+void AppendUint64Binary(const uint64_t number, FILE*& f);
+const int32_t ReadInt32Binary(FILE*& f);
+const uint32_t ReadUint32Binary(FILE*& f);
+void AppendInt32Binary(const int32_t number, FILE*& f);
+void AppendUint32Binary(const uint32_t number, FILE*& f);
+const int16_t ReadInt16Binary(FILE*& f);
+const uint16_t ReadUint16Binary(FILE*& f);
+void AppendInt16Binary(const int16_t number, FILE*& f);
+void AppendUint16Binary(const uint16_t number, FILE*& f);
+const int8_t ReadInt8Binary(FILE*& f);
+const uint8_t ReadUint8Binary(FILE*& f);
+void AppendInt8Binary(const int8_t number, FILE*& f);
+void AppendUint8Binary(const uint8_t number, FILE*& f);
 
 
 // read all characters (wide characters) from file to std::wstring
@@ -55,6 +69,11 @@ const std::wstring ReadWideContent(const std::string& filePath)
 void WriteWideContent(const std::wstring& str, const std::string& filePath)
 {
     std::ofstream outFile(filePath);
+    if (!outFile.is_open()) {
+        std::cout << "Error: File " << filePath << " doesn't exist" << std::endl;
+        return;
+    }
+
     std::wstring_convert<std::codecvt_utf8<wchar_t>> converter;
     std::string utf8Str = converter.to_bytes(str);
     outFile.write(utf8Str.c_str(), utf8Str.size());
@@ -110,34 +129,122 @@ void AppendWideCharBinary(const wchar_t c, FILE*& f)
 
 // ==========================================================================================================
 
-// read standart int number from file in binary mode
-const int ReadInt32Binary(FILE*& f)
+// read 64 bit int number from file in binary mode
+const int64_t ReadInt64Binary(FILE*& f)
 {
-    int number;
-    fread(&number, sizeof(int), 1, f);
+    int64_t number;
+    fread(&number, sizeof(int64_t), 1, f);
     return number;
 }
 
-// append standart int number to file in binary mode
-void AppendInt32Binary(const int number, FILE*& f)
+// read 64 bit unsigned int number from file in binary mode
+const uint64_t ReadUint64Binary(FILE*& f)
 {
-    fwrite(&number, sizeof(int), 1, f);
+    uint64_t number;
+    fread(&number, sizeof(uint64_t), 1, f);
+    return number;
+}
+
+// append 64 bit int number to file in binary mode
+void AppendInt64Binary(const int64_t number, FILE*& f)
+{
+    fwrite(&number, sizeof(int64_t), 1, f);
+}
+
+// append 64 bit unsigned int number to file in binary mode
+void AppendUint64Binary(const uint64_t number, FILE*& f)
+{
+    fwrite(&number, sizeof(uint64_t), 1, f);
 }
 
 // ==========================================================================================================
 
-// read number from file in binary mode
-const int ReadInt8Binary(FILE*& f)
+// read 32 bit int number from file in binary mode
+const int32_t ReadInt32Binary(FILE*& f)
+{
+    int32_t number;
+    fread(&number, sizeof(int32_t), 1, f);
+    return number;
+}
+
+// read 32 bit unsigned int number from file in binary mode
+const uint32_t ReadUint32Binary(FILE*& f)
+{
+    uint32_t number;
+    fread(&number, sizeof(uint32_t), 1, f);
+    return number;
+}
+
+// append 32 bit int number to file in binary mode
+void AppendInt32Binary(const int32_t number, FILE*& f)
+{
+    fwrite(&number, sizeof(int32_t), 1, f);
+}
+
+// append 32 bit unsigned int number to file in binary mode
+void AppendUint32Binary(const uint32_t number, FILE*& f)
+{
+    fwrite(&number, sizeof(uint32_t), 1, f);
+}
+
+// ==========================================================================================================
+
+// read 16 bit int number from file in binary mode
+const int16_t ReadInt16Binary(FILE*& f)
+{
+    int16_t number;
+    fread(&number, sizeof(int16_t), 1, f);
+    return number;
+}
+
+// read 16 bit unsigned int number from file in binary mode
+const uint16_t ReadUint16Binary(FILE*& f)
+{
+    uint16_t number;
+    fread(&number, sizeof(uint16_t), 1, f);
+    return number;
+}
+
+// append 16 bit int number to file in binary mode
+void AppendInt16Binary(const int16_t number, FILE*& f)
+{
+    fwrite(&number, sizeof(int16_t), 1, f);
+}
+
+// append 16 bit unsigned int number to file in binary mode
+void AppendUint16Binary(const uint16_t number, FILE*& f)
+{
+    fwrite(&number, sizeof(uint16_t), 1, f);
+}
+
+// ==========================================================================================================
+
+// read 8 bit int number from file in binary mode
+const int8_t ReadInt8Binary(FILE*& f)
 {
     int8_t number;
     fread(&number, sizeof(int8_t), 1, f);
     return number;
 }
 
-// append number to file in binary mode
+// read 8 bit unsigned int number from file in binary mode
+const uint8_t ReadUint8Binary(FILE*& f)
+{
+    uint8_t number;
+    fread(&number, sizeof(uint8_t), 1, f);
+    return number;
+}
+
+// append 8 bit int number to file in binary mode
 void AppendInt8Binary(const int8_t number, FILE*& f)
 {
     fwrite(&number, sizeof(int8_t), 1, f);
+}
+
+// append 8 bit unsigned int number to file in binary mode
+void AppendUint8Binary(const uint8_t number, FILE*& f)
+{
+    fwrite(&number, sizeof(uint8_t), 1, f);
 }
 
 // ==========================================================================================================
