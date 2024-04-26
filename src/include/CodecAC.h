39e832a082753437e2b723aca4ba34ea978d3ea8
aFroloff <frolovalexander993@gmail.com>
2024-04-26 18:04:40 +0300

    Update CodecAC

--- src/include/CodecAC.h
+++ src/include/CodecAC.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <queue>
 #include <map>
+#include <cmath> // for std::trunc
 
 #include "FileUtils.h"
 #include "CodecUTF8.h"
@@ -44,8 +45,7 @@ CodecAC::data_local CodecAC::Getdata_local(const std::u32string& inputStr)
 {
     // initialize sorted alphabet and sorted frequencies
     std::u32string alphabet = GetAlphabet(inputStr);
-    uint8_t alphabetLength = alphabet.size();
-    std::map<char32_t, double> charFrequenciesMap = GetCharFrequenciesMap(alphabet, alphabetLength, inputStr);
+    std::map<char32_t, double> charFrequenciesMap = GetCharFrequenciesMap(alphabet, alphabet.size(), inputStr);
     // convert map to vector
     std::vector<std::pair<char32_t, double>> charFrequenciesVector(charFrequenciesMap.begin(), charFrequenciesMap.end());
     // sort vector by frequencies
@@ -54,25 +54,24 @@ CodecAC::data_local CodecAC::Getdata_local(const std::u32string& inputStr)
     });
 
     // leave in frequencies only 2 digits after the decimal point
-    // for correct work of algorithm
     for (auto& elem : charFrequenciesVector) {
-        elem.second = static_cast<uint8_t>((elem.second * 100)) / 100.0;
+        elem.second = std::trunc((elem.second * 100.0)) / 100.0;
     }
 
     // inicialize segments
     //// (array of bounds from 0 to 1)
-    std::vector<double> segments; segments.reserve(alphabetLength + 1);
+    std::vector<double> segments; segments.reserve(alphabet.size() + 1);
     segments.push_back(0.0);
-    for (uint8_t i = 1; i < alphabetLength; ++i) {
+    for (size_t i = 1; i < alphabet.size(); ++i) {
         segments.push_back(charFrequenciesVector[i - 1].second + segments[i - 1]);
     }
     segments.push_back(1.0);
 
     // encode (get final left and right bounds)
-    double leftBound = 0, rightBound = 1, distance;
-    uint8_t index;
+    long double leftBound = 0, rightBound = 1, distance;
+    size_t index;
     for (char32_t c : inputStr) {
-        for (index = 0; index < alphabetLength; ++index) {
+        for (index = 0; index < alphabet.size(); ++index) {
             if (charFrequenciesVector[index].first == c) {
                 break;
             }
@@ -83,30 +82,30 @@ CodecAC::data_local CodecAC::Getdata_local(const std::u32string& inputStr)
         leftBound = leftBound + segments[index] * distance;
     }
 
-    double resultValueDouble = rightBound / 2 + leftBound / 2;
+    long double resultValueDouble = rightBound / (long double)(2.0) + leftBound / (long double)(2.0);
     // leave only significant fractional part of a number
-    uint64_t resultValue = static_cast<uint64_t>(resultValueDouble * 10000000000000000000.0);
+    uint64_t resultValue = resultValueDouble * 1e17;
 
     // make alphabet
     std::u32string alphabetLocal;
-    for (uint8_t i = 0; i < alphabetLength; ++i) {
+    for (size_t i = 0; i < alphabet.size(); ++i) {
         alphabetLocal.push_back(charFrequenciesVector[i].first);
     }
 
     // make frequencies vector
     std::vector<uint8_t> frequencies;
-    for (uint8_t i = 0; i < alphabetLength; ++i) {
+    for (size_t i = 0; i < alphabet.size(); ++i) {
         // leave only significant fractional part of a number
-        frequencies.push_back(static_cast<uint8_t>(charFrequenciesVector[i].second * 100));
+        frequencies.push_back(static_cast<uint8_t>(charFrequenciesVector[i].second * 100.0));
     }
 
-    return data_local(alphabetLength, alphabetLocal, frequencies, resultValue);
+    return data_local(alphabetLocal.size(), alphabetLocal, frequencies, resultValue);
 }
 
 CodecAC::data CodecAC::GetData(const std::u32string& inputStr)
 {
     // maximum number of character in the string to make encoding 
-    const uint8_t numChars = 13;
+    const uint8_t numChars = 14;
     std::queue<data_local> queueLocalData; 
 
     uint64_t strLength = inputStr.size();
@@ -129,46 +128,47 @@ CodecAC::data CodecAC::GetData(const std::u32string& inputStr)
 
 std::string CodecAC::DecodeAC(FILE* inputFile)
 {
+    // maximum number of character in the string to make local encoding 
+    const uint8_t numChars = 14;
+
     uint64_t strLength = FileUtils::ReadValueBinary<uint64_t>(inputFile);
-    // maximum number of character in the string to make encoding 
-    const uint8_t numChars = 13;
     // number of sequences to encode
     uint64_t numberOfFullSequences = (strLength % numChars == 0) ? (strLength / numChars) : (strLength / numChars);
 
     // counter of decoded sequences
-    uint64_t CountOfSeq = 0;
+    uint64_t seqsCounter = 0;
     std::string result;
 
     uint8_t alphabetLength;
     std::u32string alphabet;
     std::u32string result_local; result_local.reserve(numChars);
-    double resultValue;
-    while (CountOfSeq < numberOfFullSequences) {
+    long double resultValue;
+    while (seqsCounter < numberOfFullSequences) {
         // read values
         alphabetLength = FileUtils::ReadValueBinary<uint8_t>(inputFile);
         alphabet = CodecUTF8::DecodeString32FromBinaryFile(inputFile, alphabetLength);
         std::vector<double> frequencies; frequencies.reserve(alphabetLength);
         for (uint8_t i = 0; i < alphabetLength; ++i) {
-            frequencies.push_back(FileUtils::ReadValueBinary<uint8_t>(inputFile) / 100.0);
+            frequencies.push_back(static_cast<double>(FileUtils::ReadValueBinary<uint8_t>(inputFile)) / 100.0);
         }
 
-        resultValue = FileUtils::ReadValueBinary<uint64_t>(inputFile) / 10000000000000000000.0;
+        resultValue = static_cast<long double>(FileUtils::ReadValueBinary<uint64_t>(inputFile)) / static_cast<long double>(1e17);
 
         // decoding
         // inicialize segments
-        std::vector<double> segments; segments.reserve(alphabetLength + 1);
+        std::vector<double> segments; segments.reserve(alphabet.size() + 1);
         segments.push_back(0.0);
-        for (uint32_t i = 1; i < alphabetLength; ++i) {
+        for (size_t i = 1; i < alphabet.size(); ++i) {
             segments.push_back(frequencies[i - 1] + segments[i - 1]);
         }
         segments.push_back(1.0);
 
         // decode
-        double leftBound = 0, rightBound = 1, distance;
-        int index;
+        long double leftBound = 0, rightBound = 1, distance;
+        size_t index;
         for (uint8_t i = 0; i < numChars; ++i) {
             // find index of segment contains resultValue
-            for (uint8_t j = 0; j < alphabetLength; ++j) {
+            for (size_t j = 0; j < alphabet.size(); ++j) {
                 if (resultValue >= (leftBound + segments[j] * (rightBound - leftBound)) && 
                     resultValue < (leftBound + segments[j + 1] * (rightBound - leftBound))) {
                     index = j;
@@ -184,7 +184,7 @@ std::string CodecAC::DecodeAC(FILE* inputFile)
 
         result += CodecUTF8::EncodeString32ToString(result_local);
         result_local.clear();
-        ++CountOfSeq;
+        ++seqsCounter;
     }
     // handle the rest
     // (same code but with different count of iterations in while loop)
@@ -194,25 +194,25 @@ std::string CodecAC::DecodeAC(FILE* inputFile)
         alphabet = CodecUTF8::DecodeString32FromBinaryFile(inputFile, alphabetLength);
         std::vector<double> frequencies; frequencies.reserve(alphabetLength);
         for (uint8_t i = 0; i < alphabetLength; ++i) {
-            frequencies.push_back(FileUtils::ReadValueBinary<uint8_t>(inputFile) / 100.0);
+            frequencies.push_back(static_cast<double>(FileUtils::ReadValueBinary<uint8_t>(inputFile)) / 100.0);
         }
-        resultValue = FileUtils::ReadValueBinary<uint64_t>(inputFile) / 10000000000000000000.0;
+        resultValue = static_cast<long double>(FileUtils::ReadValueBinary<uint64_t>(inputFile)) / static_cast<long double>(1e17);
 
         // decoding
         // inicialize segments
-        std::vector<double> segments; segments.reserve(alphabetLength + 1);
-        segments.push_back(0);
-        for (uint32_t i = 1; i < alphabetLength; ++i) {
+        std::vector<double> segments; segments.reserve(alphabet.size() + 1);
+        segments.push_back(0.0);
+        for (size_t i = 1; i < alphabet.size(); ++i) {
             segments.push_back(frequencies[i - 1] + segments[i - 1]);
         }
-        segments.push_back(1);
+        segments.push_back(1.0);
 
         // decode
-        double leftBound = 0, rightBound = 1, distance;
-        int index;
-        for (uint8_t i = 0; i < strLength % numChars; ++i) {
+        long double leftBound = 0, rightBound = 1, distance;
+        size_t index;
+        for (uint64_t i = 0; i < strLength % numChars; ++i) {
             // find index of segment contains resultValue
-            for (uint8_t j = 0; j < alphabetLength; ++j) {
+            for (size_t j = 0; j < alphabet.size(); ++j) {
                 if (resultValue >= (leftBound + segments[j] * (rightBound - leftBound)) && 
                     resultValue < (leftBound + segments[j + 1] * (rightBound - leftBound))) {
                     index = j;
@@ -227,7 +227,7 @@ std::string CodecAC::DecodeAC(FILE* inputFile)
         }
 
         result += CodecUTF8::EncodeString32ToString(result_local);
-        ++CountOfSeq;
+        ++seqsCounter;
     }
 
     return result;
